bf86d5744d86a1298132202d37bcb0bc60803a11
BGirginn <girginbora30@gmail.com>
2025-08-19 17:32:26 +0300

    update

--- structers/target/debug/deps/structers.d
+++ structers/target/debug/deps/structers.d
@@ -1,5 +1,5 @@
-D:\Bora\Yazilim\Rust\structers\target\debug\deps\structers.d: src\main.rs
+d:\Bora\Yazilim\Rust\structers\target\debug\deps\structers.d: src\main.rs
 
-D:\Bora\Yazilim\Rust\structers\target\debug\deps\structers.exe: src\main.rs
+d:\Bora\Yazilim\Rust\structers\target\debug\deps\structers.exe: src\main.rs
 
 src\main.rs:
