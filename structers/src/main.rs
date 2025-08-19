74e74d1d851b05d30ecafd8ef884a081c913954a
BGirginn <girginbora30@gmail.com>
2025-08-19 23:30:03 +0300

    update

--- structers/src/main.rs
+++ structers/src/main.rs
@@ -65,7 +65,17 @@ fn main() {
     println!("X: {}", origin.0);
 
 
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
 
 }
 
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
 
